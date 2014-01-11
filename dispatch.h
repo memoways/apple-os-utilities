2bbc5c6158324ef0dde2fa305293b9207d317afa
fabrice truillot de chambrier <fabrice@meninsilicium.com>
2014-01-12 00:15:57 +0100

    added dispatch_async_on_global_queue helper

--- dispatch.h
+++ dispatch.h
@@ -10,5 +10,11 @@
 DISPATCH_EXPORT DISPATCH_NONNULL_ALL
 void dispatch_async_on_main_queue( dispatch_block_t block );
 
+DISPATCH_INLINE DISPATCH_NONNULL_ALL DISPATCH_NOTHROW
+void dispatch_async_on_global_queue( dispatch_block_t block )
+{
+	dispatch_async( dispatch_get_global_queue( DISPATCH_QUEUE_PRIORITY_DEFAULT, 0 ), block );
+}
+
 DISPATCH_EXPORT DISPATCH_NONNULL_ALL
 void dispatch_sync_on_main_queue( dispatch_block_t block );
